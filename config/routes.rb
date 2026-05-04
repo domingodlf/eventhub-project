99da03eff5a6f68d08ef4a482346889d19023925
Domingo De la Fuente <ddelafuente@miuandes.cl>
2026-05-04 18:42:02 -0400

    Add EventHub landing page

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :registrations, only: [:index, :show]
   resources :reviews, only: [:index, :show]
 
-  root "events#index"
+  root "home#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
